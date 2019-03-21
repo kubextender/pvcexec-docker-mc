3ce4ce3de852af8f2c2788b418ba557e80caa1ed
Dragan Ljubojevic <dragan.ljubojevic@gmail.com>
2019-03-21 20:11:52 +0100

    fix ci file

--- docker_push.sh
+++ docker_push.sh
@@ -2,5 +2,4 @@
 IMAGE_NAME="$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG"
 echo "Building docker image $IMAGE_NAME"
 docker build -t "$IMAGE_NAME" .
-echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USER" --password-stdin
 docker push "$IMAGE_NAME"
